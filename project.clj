a42d1150a9a39bcf9f3d663991da922c9ba8b3dc
Martin Jul <martin@mjul.com>
2010-09-27 18:48:07 +0200

    Added cukes.info repository for getting Cucumber.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject org.clojars.mjul/lein-cuke "0.3.2"
   :description "Cucumber runner for Leiningen (lein)"
+  :repositories {"cukes", "http://cukes.info/maven"}
   :dependencies [[leiningen "1.3.1"]
                  [cuke4duke "0.3.2"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
