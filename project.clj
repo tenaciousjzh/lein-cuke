fa98b8add1c27b3b823f9ef2a49448047f877d66
Martin Jul <martin@mjul.com>
2010-09-27 13:59:17 +0200

    Updated name to publish jar under under org.clojars.mjul groupId.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-cuke "0.3.2-SNAPSHOT"
+(defproject org.clojars.mjul/lein-cuke "0.3.2"
   :description "Cucumber runner for Leiningen (lein)"
   :dependencies [[leiningen "1.3.1"]
-                 [lein-clojars "0.6.0"]
-                 [cuke4duke "0.3.2"]])
+                 [cuke4duke "0.3.2"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
 
